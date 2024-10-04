5bfa0dced6b7eddd6c3297230a8b0213dfaccd1e
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-10-04 15:13:10 -0300

    add login system

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ webserver.get('/area-do-cliente', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/html/area-do-cliente.html'));
 })
 
+webserver.post('/login-endpoint', (req, res) => {
+    res.send('Login realizado com sucesso');
+})
+
 webserver.listen(port, () => {
     console.log('Server is running on port 5050');
 
