c2edc33f0ae79cb957f921570db6d6aebc97597b
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-10-27 17:43:20 -0300

    update project

--- src/static/javascript/sidebar.js
+++ src/static/javascript/sidebar.js
@@ -4,11 +4,36 @@ function loadContent(page) {
     if (page === 'relatorios') {
         contentDiv.innerHTML = '<h1>Oi, você está nos Relatórios!</h1>';
     } else if (page === 'agenda') {
-        contentDiv.innerHTML = '<h1>Oi, você está na Agenda!</h1>';
+        contentDiv.innerHTML = `
+            <h1>Agenda de Consultas</h1>
+            <div id="consultation-list" class="consultation-list"></div>
+        `;
+
+        fetch('/get-list')
+            .then(response => response.json())
+            .then(data => {
+                const consultationList = document.getElementById('consultation-list');
+                if (data.length > 0) {
+                    consultationList.innerHTML = data.map(consulta => `
+                        <div class="consultation-item">
+                            <p><strong>Data:</strong> ${consulta.data_atendimento}</p>
+                            <p><strong>Horário:</strong> ${consulta.horario_inicio} - ${consulta.horario_fim}</p>
+                            <p><strong>Paciente:</strong> ${consulta.nome_paciente}</p>
+                            <p><strong>Médico:</strong> ${consulta.nome_medico}</p>
+                        </div>
+                    `).join('');
+                } else {
+                    consultationList.innerHTML = '<p>Sem consultas agendadas.</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao carregar a lista de consultas:', error);
+                document.getElementById('consultation-list').innerHTML = '<p>Erro ao carregar a lista de consultas.</p>';
+            });
     } else {
         contentDiv.innerHTML = `
             <h1>Escolha uma Data e Hora</h1>
-            <input type="text" id="datetimepicker">
+            <input type="text" id="datetimepicker" class="date-input">
             <button id="submitBtn">Enviar</button>
         `;
 
@@ -46,9 +71,9 @@ function loadContent(page) {
 window.onload = function() {
     const currentHash = window.location.hash.substring(1);
     loadContent(currentHash); 
-}
+};
 
 window.onhashchange = function() {
     const currentHash = window.location.hash.substring(1);
     loadContent(currentHash);
-}
+};
